2603e671a9baae750f3fe6aad7ca16e2c3c74253
KP Dorsey <dorsey_k17@hotmail.com>
2026-02-26 13:13:16 -0600

    Professional Portfolio Framework Draft 1.0

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+}
+
+function toggleSidebar() {
+  document.getElementById('sidebar').classList.toggle('open');
+}
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+
+  const btn = document.getElementById('themeToggle');
+
+  if (document.body.classList.contains('dark')) {
+    btn.textContent = "Light Theme";
+  } else {
+    btn.textContent = "Dark Theme";
+  }
+}
